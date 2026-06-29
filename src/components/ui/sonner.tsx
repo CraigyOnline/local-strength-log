65ddbe1379fefeb91769d6874f09e36504b8cda7
CraigyOnline <127735344+CraigyOnline@users.noreply.github.com>
2026-06-29 21:31:16 +0100

    Add files via upload

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -5,6 +5,7 @@ type ToasterProps = React.ComponentProps<typeof Sonner>;
 const Toaster = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
+      duration={4000}
       className="toaster group"
       toastOptions={{
         classNames: {
